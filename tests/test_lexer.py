d4c7e2641ce80c5d5be0494afccc6a73c112984e
Nick Barrios <nick@nicholasbarrios.com>
2022-03-05 22:54:03 -0800

    Add lexer test for variable declaration

--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -1,5 +1,23 @@
-import antlr.generated.PeaceLexer
+from __future__ import unicode_literals
+import sys
 import unittest
+import antlr4
+
+sys.path.append("../")
+from antlr.generated.PeaceLexer import PeaceLexer
+
+class TestLexer(unittest.TestCase):
+    def test_declare(self):
+        test_input = "let x: int = 20;"
+        test_expected_text = ['let', 'x', ':', 'int', '=', '20', ';']
+        test_expected_types = [17, 38, 30, 1, 28, 40, 31] 
+        input_stream = antlr4.InputStream(test_input)
+        lexer = PeaceLexer(input_stream)
+        tokens = lexer.getAllTokens()
+        token_text = list(map(lambda x: x.text, tokens))
+        token_types = list(map(lambda x: x.type, tokens))
+        assert(token_text == test_expected_text)
+        assert(token_types == test_expected_types)
 
 if __name__ == '__main__':
     unittest.main()
