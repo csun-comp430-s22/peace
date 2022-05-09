6bd023f9bff44814e2fd09479e353a1f6213597e
Nick Barrios <nicholas.barrios.60@my.csun.edu>
2022-05-09 15:00:05 -0700

    Added arithmetic expression, assign, and comparison tests. Implemented enough of the typechecker to get those tests to pass.

--- tests/TestTypechecker.py
+++ tests/TestTypechecker.py
@@ -8,7 +8,7 @@ from antlr4.tree.Trees import Trees
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from antlr.generated.PeaceLexer import PeaceLexer
 from antlr.generated.PeaceParser import PeaceParser
-from typechecker import PeaceTypechecker
+from typechecker.PeaceTypechecker import PeaceTypechecker, PeaceTypecheckError
 
 from TestParserListener import TestListener
 
@@ -19,28 +19,110 @@ def create_parser_for(input):
     stream = CommonTokenStream(lexer)
     return PeaceParser(stream)
 
-def parser_test(tree):
-    listener = PeaceTypechecker()
-    walker = ParseTreeWalker()
-    walker.walk(listener, tree)
-    return listener.rules_visited
+def typecheck_tree(tree):
+    visitor = PeaceTypechecker()
+    visitor.visit(tree)
 
 class TestParser(unittest.TestCase):
-    def test_arithmetic_expression(self):
+    def test_arithmetic_expression_ints(self):
         test_input = """
-enum foo {
-    int bar,
-    int baz
-}
-
-void main() {
-    
-}
+        1 + 2
         """
-        tree_expected = ''
         parser = create_parser_for(test_input)
-        tree = parser.program()
-        #assert(tree_expected == tree_actual)
+        tree = parser.expression()
+        typecheck_tree(tree)
+
+    def test_arithmetic_expression_floats(self):
+        test_input = """
+        3.0 + 100.0
+        """
+        parser = create_parser_for(test_input)
+        tree = parser.expression()
+        typecheck_tree(tree)
+
+    def test_arithmetic_expression_mixed(self):
+        test_input = """
+        2 + 10.9
+        """
+        parser = create_parser_for(test_input)
+        tree = parser.expression()
+        typecheck_tree(tree)
+
+    def test_arithmetic_expression_invalid(self):
+        test_input = """
+        2 + true
+        """
+        parser = create_parser_for(test_input)
+        tree = parser.expression()
+        with self.assertRaises(PeaceTypecheckError):
+            typecheck_tree(tree)
+
+    def test_assign_arithmetic_expression(self):
+        test_input = """
+        {
+            let a: int = 1;
+            let b: int = a + 2;
+        }
+        """
+        parser = create_parser_for(test_input)
+        tree = parser.block()
+        typecheck_tree(tree)
+
+    def test_comparison_expression_ints(self):
+        test_input = """
+        1 > 2
+        """
+        parser = create_parser_for(test_input)
+        tree = parser.expression()
+        typecheck_tree(tree)
+
+    def test_comparison_expression_floats(self):
+        test_input = """
+        1.9 < 2.1
+        """
+        parser = create_parser_for(test_input)
+        tree = parser.expression()
+        typecheck_tree(tree)
+
+    def test_comparison_expression_mixed(self):
+        test_input = """
+        6 >= 2
+        """
+        parser = create_parser_for(test_input)
+        tree = parser.expression()
+        typecheck_tree(tree)
+
+    def test_comparison_expression_invalid(self):
+        test_input = """
+        foo <= 2
+        """
+        parser = create_parser_for(test_input)
+        tree = parser.expression()
+        with self.assertRaises(PeaceTypecheckError):
+            typecheck_tree(tree)
+
+    def test_assign(self):
+        test_input = """
+        {
+            let a: int = 1;
+            a = 2;
+        }
+        """
+        parser = create_parser_for(test_input)
+        tree = parser.block()
+        typecheck_tree(tree)
+
+    def test_assign_invalid(self):
+        test_input = """
+        {
+            let a: bool = false;
+            a = 3.14;
+        }
+        """
+        parser = create_parser_for(test_input)
+        tree = parser.block()
+        with self.assertRaises(PeaceTypecheckError):
+            typecheck_tree(tree)
 
 if __name__ == '__main__':
     unittest.main()
--- typechecker/PeaceTypechecker.py
+++ typechecker/PeaceTypechecker.py
@@ -1,25 +1,205 @@
+import copy
 from antlr.generated.PeaceParser import PeaceParser
-from antlr.generated.PeaceListener import PeaceListener
+from antlr.generated.PeaceVisitor import PeaceVisitor
+
+class PeaceType():
+    def __init__(self, token_type, token):
+        self.token_type = token_type
+        self.token = token
+
+    def __eq__(self, other) -> bool:
+        return (self.token_type == other.token_type and self.token == other.token)
 
 class PeaceTypecheckError(Exception):
     def __init__(self, message):
         self.message = message
 
-class PeaceTypechecker(PeaceListener):
+class PeaceTypechecker(PeaceVisitor):
     def __init__(self) -> None:
+        self.type_environments = []
         super().__init__()
 
-    # Enter a parse tree produced by PeaceParser#ArithmeticExpr.
-    def exitArithmeticExpr(self, ctx:PeaceParser.ArithmeticExprContext):
-        print(ctx.op().getType())
+    # Visit a parse tree produced by PeaceParser#basetype.
+    def visitBasetype(self, ctx:PeaceParser.BasetypeContext):
+        return self.visitChildren(ctx)
 
 
-    # Enter a parse tree produced by PeaceParser#DigitExpr.
-    def exitDigitExpr(self, ctx:PeaceParser.DigitExprContext):
-        pass
+    # Visit a parse tree produced by PeaceParser#funcpointertype.
+    def visitFuncpointertype(self, ctx:PeaceParser.FuncpointertypeContext):
+        return self.visitChildren(ctx)
 
-    # Enter a parse tree produced by PeaceParser#IdentExpr.
-    def exitIdentExpr(self, ctx:PeaceParser.IdentExprContext):
-        ctx.getToken().toString()
 
-        
+    # Visit a parse tree produced by PeaceParser#atype.
+    def visitAtype(self, ctx:PeaceParser.AtypeContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by PeaceParser#op.
+    def visitOp(self, ctx:PeaceParser.OpContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by PeaceParser#EnumExpr.
+    def visitEnumExpr(self, ctx:PeaceParser.EnumExprContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by PeaceParser#BoolExpr.
+    def visitBoolExpr(self, ctx:PeaceParser.BoolExprContext):
+        return PeaceType(PeaceParser.Bool, 'bool')
+
+
+    # Visit a parse tree produced by PeaceParser#IdentExpr.
+    def visitIdentExpr(self, ctx:PeaceParser.IdentExprContext):
+        if (self.type_environments and
+            ctx.Identifier().getText() in self.type_environments[-1]):
+            return self.type_environments[-1][ctx.Identifier().getText()]
+        return PeaceType(PeaceParser.Identifier, ctx.Identifier().getText())
+
+
+    # Visit a parse tree produced by PeaceParser#FloatExpr.
+    def visitFloatExpr(self, ctx:PeaceParser.FloatExprContext):
+        return PeaceType(PeaceParser.Float, 'float')
+
+    # Visit a parse tree produced by PeaceParser#ArithmeticExpr.
+    def visitArithmeticExpr(self, ctx:PeaceParser.ArithmeticExprContext):
+        l_type = self.visit(ctx.expression(0))
+        r_type = self.visit(ctx.expression(1))
+        if (l_type.token_type != PeaceParser.Int and l_type.token_type != PeaceParser.Float):
+            raise PeaceTypecheckError("Invalid type for arithmetic expression: " + l_type.token)
+        if (r_type.token_type != PeaceParser.Int and r_type.token_type != PeaceParser.Float):
+            raise PeaceTypecheckError("Invalid type for arithmetic expression: " + r_type.token)
+        if (l_type.token_type == PeaceParser.Float):
+            return l_type
+        if (r_type.token_type == PeaceParser.Float):
+            return r_type
+        return l_type
+
+
+    # Visit a parse tree produced by PeaceParser#DigitExpr.
+    def visitDigitExpr(self, ctx:PeaceParser.DigitExprContext):
+        return PeaceType(PeaceParser.Int, 'int')
+
+
+    # Visit a parse tree produced by PeaceParser#CompExpr.
+    def visitCompExpr(self, ctx:PeaceParser.CompExprContext):
+        l_type = self.visit(ctx.expression(0))
+        r_type = self.visit(ctx.expression(1))
+        if (l_type.token_type != PeaceParser.Int and l_type.token_type != PeaceParser.Float):
+            raise PeaceTypecheckError("Invalid type for comparison: " + l_type.token)
+        if (r_type.token_type != PeaceParser.Int and r_type.token_type != PeaceParser.Float):
+            raise PeaceTypecheckError("Invalid type for comparison: " + r_type.token)
+        return PeaceType(PeaceParser.Bool, 'bool')
+
+
+    # Visit a parse tree produced by PeaceParser#FuncPointCreateExpr.
+    def visitFuncPointCreateExpr(self, ctx:PeaceParser.FuncPointCreateExprContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by PeaceParser#AssignExpr.
+    def visitAssignExpr(self, ctx:PeaceParser.AssignExprContext):
+        l_type = self.visit(ctx.expression(0))
+        r_type = self.visit(ctx.expression(1))
+        if (l_type != r_type):
+            raise PeaceTypecheckError("Assignment type mismatch: " + l_type.token + " and " + r_type.token)
+        return l_type
+
+
+    # Visit a parse tree produced by PeaceParser#FuncCallExpr.
+    def visitFuncCallExpr(self, ctx:PeaceParser.FuncCallExprContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by PeaceParser#vardec.
+    def visitVardec(self, ctx:PeaceParser.VardecContext):
+        r_type = self.visit(ctx.expression())
+        l_type = PeaceType(ctx.atype().start.type, ctx.atype().getText())
+        if (r_type != l_type):
+            raise PeaceTypecheckError("Variable declaration type mismatch: " + l_type.token + " and " + r_type.token)
+        self.type_environments[-1][ctx.Identifier().getText()] =  l_type
+
+
+    # Visit a parse tree produced by PeaceParser#ExprStmt.
+    def visitExprStmt(self, ctx:PeaceParser.ExprStmtContext):
+        return self.visit(ctx.expression())
+
+
+    # Visit a parse tree produced by PeaceParser#VarDecStmt.
+    def visitVarDecStmt(self, ctx:PeaceParser.VarDecStmtContext):
+        return self.visit(ctx.vardec())
+
+
+    # Visit a parse tree produced by PeaceParser#WhileStmt.
+    def visitWhileStmt(self, ctx:PeaceParser.WhileStmtContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by PeaceParser#IfStmt.
+    def visitIfStmt(self, ctx:PeaceParser.IfStmtContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by PeaceParser#MatchStmt.
+    def visitMatchStmt(self, ctx:PeaceParser.MatchStmtContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by PeaceParser#ReturnExprStmt.
+    def visitReturnExprStmt(self, ctx:PeaceParser.ReturnExprStmtContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by PeaceParser#ReturnStmt.
+    def visitReturnStmt(self, ctx:PeaceParser.ReturnStmtContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by PeaceParser#PrintStmt.
+    def visitPrintStmt(self, ctx:PeaceParser.PrintStmtContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by PeaceParser#block.
+    def visitBlock(self, ctx:PeaceParser.BlockContext):
+        new_type_environment = dict()
+        if (self.type_environments):
+            new_type_environment = copy.deepcopy(self.type_environments[-1])
+        self.type_environments.append(new_type_environment)
+        self.visitChildren(ctx)
+        self.type_environments.pop()
+
+
+    # Visit a parse tree produced by PeaceParser#case_.
+    def visitCase_(self, ctx:PeaceParser.Case_Context):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by PeaceParser#pattern.
+    def visitPattern(self, ctx:PeaceParser.PatternContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by PeaceParser#parameter.
+    def visitParameter(self, ctx:PeaceParser.ParameterContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by PeaceParser#func_stmt.
+    def visitFunc_stmt(self, ctx:PeaceParser.Func_stmtContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by PeaceParser#cdef.
+    def visitCdef(self, ctx:PeaceParser.CdefContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by PeaceParser#enumdef.
+    def visitEnumdef(self, ctx:PeaceParser.EnumdefContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by PeaceParser#program.
+    def visitProgram(self, ctx:PeaceParser.ProgramContext):
+        return self.visitChildren(ctx)
